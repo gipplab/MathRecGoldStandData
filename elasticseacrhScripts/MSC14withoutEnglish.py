d201cdef995779ee46070ccbd23667bf546177e7
AnkitSatpute <ankitsureshsatpute@gmail.com>
2022-10-25 11:54:27 +0200

    explo analysis zbMATH

---
+++ elasticseacrhScripts/MSC14withoutEnglish.py
@@ -0,0 +1,33 @@
+import dill as pickle
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
+
+ohneEngMSC14 = es.search(index="temp_zbmath_my",
+	body={"_source": ["id"],
+	"size": 15000,
+ 	"query": {
+   		"bool": {
+     	"must": [
+        	{ "wildcard": { "msc.code": "14*" } }
+      	],
+     	"must_not": [{ 
+       		"wildcard": {
+     		"reviews.language": "English"
+    		}
+    	}]
+ 	}
+ 	}},
+	sort = ["id"])
+
+print(len(ohneEngMSC14["hits"]["hits"]))
+
+listofIDS = list()
+
+for everyEle in ohneEngMSC14["hits"]["hits"]:
+	listofIDS.append(everyEle["_id"])
+
+print(listofIDS[1200])
+
+with open('ohneEngIDS.pkl', 'wb') as fr:
+	pickle.dump(listofIDS, fr)
